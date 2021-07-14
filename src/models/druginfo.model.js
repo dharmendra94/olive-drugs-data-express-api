547a6193163f23ca412ad9ac494708ed6b77e440
Sai Dharmendra Kanneganti <Dharma@neemanalytics.com>
2021-07-14 18:47:44 -0400

    Updated drug search query

--- src/models/druginfo.model.js
+++ src/models/druginfo.model.js
@@ -1,26 +1,21 @@
 const query = require("../db/db-connection");
-const { multipleColumnSet } = require("../utils/common.utils");
 
 class DrugInfoModel {
     drugSearch = async (searchTerm) => {
         let replacement = `'%${searchTerm}%'`;
 
-        let sql = `SELECT A.NDC
-                            , B.DoseUse
-                            , B.PkgQty
-                            , B.PkgSize
-                            , B.PkgTotalUnits
-                            , B.PkgSizeUom
+        let sql = `SELECT MAX(A.NDC) NDC
                             , C.DrugName
-                            , C.DosageForm
                             , C.Strength
                             , C.DrugEquivNum
                     FROM NDCInfo A
                     LEFT JOIN PackageInfo B ON B.NDCPackageCode = A.FDA_NDCFormat
                     LEFT JOIN DrugsInfo C ON C.ProductId = B.ProductId
                     WHERE C.DrugName LIKE ${replacement} AND C.Strength IS NOT NULL
+                    GROUP BY C.DrugName, C.Strength, C.DrugEquivNum
                     ORDER BY C.DrugName 
-                    LIMIT 10`;
+                    LIMIT 10
+                    `;
 
         const result = await query(sql);
 
