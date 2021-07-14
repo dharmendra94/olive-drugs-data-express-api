31a1f1e71bbc54f22f5bab781fc9b83e49c907d9
Sai Dharmendra Kanneganti <Dharma@neemanalytics.com>
2021-07-14 14:56:58 -0400

    Updated route url

--- src/routes/nadac.route.js
+++ src/routes/nadac.route.js
@@ -3,7 +3,7 @@ const drugsinfoController = require("../controllers/drugsinfo.controller");
 const router = express.Router();
 const awaitHandlerFactory = require("../middleware/awaitHandlerFactor.middleware");
 
-router.get("/drugsearch/:drugName", awaitHandlerFactory(drugsinfoController.drugSearch)); 
+router.get("/drugSearch/:drugName", awaitHandlerFactory(drugsinfoController.drugSearch)); 
 router.get("/drugEquivalentsCost/:ndc", awaitHandlerFactory(drugsinfoController.drugEquivalentsCost));
 
 module.exports = router;
